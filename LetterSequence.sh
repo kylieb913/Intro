0ddcc95b1cd3bc7842ad0c1da2a80a09bec1ab1a
Marie Mooney <99214787+drswoop@users.noreply.github.com>
2026-06-15 19:12:53 +0000

    Empty script file for Computing 6 exercise LetterSequence in summer 2026.

---
+++ LetterSequence.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#This is where you will write your LetterSequence.sh script.
+
+#Make sure to record your thoughts and instructions in your OWN words in the comments 
+#and to use the comments to show that you can explain your code.
